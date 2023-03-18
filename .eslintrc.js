4280b64a6dc017ee45a4f1e313e02142cf877d13
Qlexal <lex999com@gmail.com>
2023-03-18 20:44:11 +0200

    build: added eslintrc and prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: ['airbnb-base'],
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: [],
+    ignorePatterns: ['.eslintrc.js', 'commitlint.config.js'],
+    rules: {
+      'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+      'import/prefer-default-export': 'off',
+      'import/named': 'off',
+      'class-methods-use-this': 'off',
+      'import/no-cycle': 'off',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+    },
+  };
